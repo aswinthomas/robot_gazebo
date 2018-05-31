14fbe0f75c6ee041a6608934a74b2ddbb3de97ee
Aswin Thomas <aswinthomas@gmail.com>
2018-05-31 13:41:58 +0800

    install script for ros

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # robot_gazebo_install
+
+## Installation
+
+- Create a virtualbox (or similar) with ubuntu 16.04 64bit
+- Then run ```./install.sh```
---
+++ install.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:apt-fast/stable
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt update
+sudo apt -y install apt-fast
+sudo apt-fast -y install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+sudo apt-fast -y install ros-kinetic-husky-simulator
+
