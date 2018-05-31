d19ed3bc58450fc95c7ec733d8e5a2e33d8edf4a
Aswin Thomas <aswinthomas@gmail.com>
2018-05-31 14:32:01 +0800

    wstool install

--- install.bash
+++ install.bash
@@ -6,7 +6,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt update
 sudo apt -y install apt-fast
-sudo apt-fast -y install ros-kinetic-desktop-full
+sudo apt-fast -y install ros-kinetic-desktop-full python-wstool
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
