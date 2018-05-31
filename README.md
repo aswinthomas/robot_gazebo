56df2d4f9c235c653b6cbc38cc1ddb6867fd66b7
Aswin Thomas <aswinthomas@gmail.com>
2018-05-31 14:14:42 +0800

    gazebo launch

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # robot_gazebo_install
 
-## Installation
+## Installation on vbox
 
 - Create a virtualbox (or similar) with ubuntu 16.04 64bit
 - Then run ```./install.sh```
+
+## Running server on vbox
+
+```roslaunch server_services.launch```
+
+## Running client on your host
+
--- install.bash
+++ install.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository -y ppa:apt-fast/stable
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
@@ -8,6 +9,8 @@ sudo apt -y install apt-fast
 sudo apt-fast -y install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 sudo apt-fast -y install ros-kinetic-jackal-simulator ros-kinetic-jackal-desktop 
 sudo apt-fast -y install ros-kinetic-rostful
 
