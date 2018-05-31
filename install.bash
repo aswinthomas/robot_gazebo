aa1c66abdeb4a1a0dd3fde2172f04d9c12b4e409
Aswin Thomas <aswinthomas@gmail.com>
2018-05-31 15:34:10 +0800

    added rostful launch

--- install.bash
+++ install.bash
@@ -5,8 +5,8 @@ sudo add-apt-repository -y ppa:apt-fast/stable
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt update
-sudo apt -y install apt-fast
-sudo apt-fast -y install ros-kinetic-desktop-full python-wstool
+sudo apt -y install apt-fast git
+sudo apt-fast -y install ros-kinetic-desktop-full python-wstool python-pip
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
@@ -14,10 +14,11 @@ source ~/.bashrc
 sudo apt-fast -y install ros-kinetic-jackal-simulator ros-kinetic-jackal-desktop 
 #sudo apt-fast -y install ros-kinetic-rostful -- does not seem to work
 
+sudo pip install rostful pyros
+
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws
-wstool init src https://raw.githubusercontent.com/asmodehn/rostful/kinetic/rosinstall/kinetic.rosinstall
-rosdep update
+cd src && git clone https://github.com/benkehoe/rostful && cd ..
 rosdep install --from-paths src --ignore-src -y
 catkin_make
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
